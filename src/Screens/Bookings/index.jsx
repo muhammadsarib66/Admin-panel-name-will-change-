35cf0aaaf5466c769e9a864036306bc1547ac5bf
muhammadsarib66 <saribnoor0310@gmail.com>
2024-02-10 19:43:15 +0500

    fix

--- src/Screens/Bookings/index.jsx
+++ src/Screens/Bookings/index.jsx
@@ -116,8 +116,8 @@ const index = () => {
                 Bookings
               </h1>
               <div>
-                <button className="bg-blue-500 text-white px-4 py-2 rounded-lg">
-                  Add Booking
+                <button className="bg-blue-600  text-white px-4 py-2 rounded-lg">
+                  Add Booking 
                 </button>
               </div>
             </div>
